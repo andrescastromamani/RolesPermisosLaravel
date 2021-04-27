305ff23bbec9b7947157604a94ead0ad2e4c09c0
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-04-27 12:36:25 -0400

    Modificamos las configuraciones del Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,8 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
-        'role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
-        'allroles' => \Caffeinated\Shinobi\Middleware\UserHasAllRoles::class,
-        'anyroles' => \Caffeinated\Shinobi\Middleware\UserHasAnyRole::class,
+        'has.role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
+        'has.permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
     ];
 }
